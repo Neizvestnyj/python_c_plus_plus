497ec8ca32c25cfd2061e6ccc319e449a99c5f0b
Neizvestnyj <pikromat1995@gmail.com>
2021-11-07 23:55:15 +0300

    added `requirements`, `python_requires`, package name argument

--- setup.py
+++ setup.py
@@ -2,16 +2,33 @@
 
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
+
+try:
+    from Cython.Build import cythonize
+except (ImportError, ModuleNotFoundError) as import_cython_error:
+    print('Please, install cython')
 
 __version__ = '0.1'
+__name__ = 'py_c_plus_plus_examples'
+
+with open('requirements.txt') as req_f:
+    requirements = []
+    lines = req_f.readlines()
+    for line in lines:
+        requirements.append(line.replace('\n', '').strip())
 
 extensions = [
-    Extension('py_c_plus_plus_examples.c_date', ['py_c_plus_plus_examples/c_date.pyx']),
-    Extension('py_c_plus_plus_examples.c_trig', ['py_c_plus_plus_examples/c_trig.pyx']),
+    Extension(f'{__name__}.c_date', [f'{__name__}/c_date.pyx']),
+    Extension(f'{__name__}.c_trig', [f'{__name__}/c_trig.pyx']),
 ]
 
-setup(name='py_c_plus_plus_examples',
+setup(name=__name__,
       version=__version__,
+      url=f'https://github.com/Neizvestnyj/{__name__}',
+      description='C++ extension for Python',
+      platforms=['all'],
+      license='GPL-3.0 License',
       ext_modules=cythonize(extensions),
-)
+      install_requires=requirements,
+      python_requires=">=3.0",
+      )
