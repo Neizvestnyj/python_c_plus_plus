de6b6bd3252a754db7a21f39f12e8d8dfc625de0
Neizvestnyj <pikromat1995@gmail.com>
2021-11-08 16:06:48 +0300

    added `author, keywords, zip_safe` arguments to `setup`

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 # https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html
+# https://github.com/Technologicat/setup-template-cython/blob/master/setup.py
 
 from distutils.core import setup
 from distutils.extension import Extension
+import sys
 
 try:
     from Cython.Build import cythonize
 except (ImportError, ModuleNotFoundError) as import_cython_error:
-    print('Please, install cython')
+    sys.exit("Cython not found. Cython is needed to build the extension modules.")
 
 __version__ = '0.1'
 __name__ = 'py_c_plus_plus_examples'
@@ -24,11 +26,17 @@ extensions = [
 
 setup(name=__name__,
       version=__version__,
+      author="Neizvestnyj and psychowasp",
       url=f'https://github.com/Neizvestnyj/{__name__}',
       description='C++ extension for Python',
       platforms=['all'],
       license='GPL-3.0 License',
+      keywords=["python c++ example using cython"],
       ext_modules=cythonize(extensions),
       install_requires=requirements,
+      # Disable zip_safe, because:
+      #   - Cython won't find `.pxd` files inside installed .egg, hard to compile libs depending on this one
+      #   - dynamic loader may need to have the library unzipped to a temporary directory anyway (at import time)
+      zip_safe=False,
       python_requires=">=3.0",
       )
