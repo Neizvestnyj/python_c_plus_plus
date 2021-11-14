68e9c8504e339c48df939adcfd37385c24bfc647
Neizvestnyj <pikromat1995@gmail.com>
2021-11-14 13:58:53 +0300

    update `dlib`

--- test/main.py
+++ test/main.py
@@ -51,5 +51,4 @@ print(f'area: {py_rect.get_area()}')
 py_rect.move(1, 2)
 print(f'size: {py_rect.get_size()}')
 
-print(PyDlibLandmark().run("/home/neizvestnyj/Downloads/data".encode(),
-                           "/home/neizvestnyj/Downloads/faces/8490.jpg".encode()))
+# PyDlibLandmark().run("<path>/shape_predictor_68_face_landmarks.dat".encode(), "<path>/face.<extension>".encode())
