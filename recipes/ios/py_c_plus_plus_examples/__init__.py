ab5bbae80ebdc67a19b7ac2728caae2c02a3ed9b
Neizvestnyj <pikromat1995@gmail.com>
2022-07-26 21:47:57 +0300

    update ios recipe

--- recipes/ios/py_c_plus_plus_examples/__init__.py
+++ recipes/ios/py_c_plus_plus_examples/__init__.py
@@ -1,12 +1,10 @@
-from kivy_ios.recipe import CythonRecipe
+from kivy_ios.toolchain import CythonRecipe
 
 
 class CPackageRecipe(CythonRecipe):
     site_packages_name = 'py_c_plus_plus_examples'
     version = '0.1'
-    # url = '' # path to setup.py
-    depends = ['setuptools', 'cython']
-    call_hostpython_via_targetpython = False
+    url = '' # path to setup.py
 
 
 recipe = CPackageRecipe()
